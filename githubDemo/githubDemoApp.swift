d1aa3ef4526fc673d17dc37368a288ddd0e1f104
Aisha alsharhan <H00444865@hct.ac.ae>
2024-06-21 09:21:19 +0400

    Initial Commit

---
+++ githubDemo/githubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubDemoApp.swift
+//  githubDemo
+//
+//  Created by Aisha AlSharhan on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct githubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
